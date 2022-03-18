9a5bf7e0250f685b7aaf5a52db11963dcc1ed5ee
rosemcc <rosemccoll@hotmail.com>
2022-03-18 17:17:57 +1300

    maybe fix the cors error

--- graphAPI.ts
+++ graphAPI.ts
@@ -2,10 +2,17 @@ import { APIGatewayProxyResult } from "aws-lambda";
 import { generateContentGraph } from "./graph";
 
 export async function getGraph(): Promise<APIGatewayProxyResult> {
+    if (!process.env.CORS_ACCESS_CONTROL_ALLOW_ORIGINS) throw Error('No CORS setting found');
+    const headers: { [header: string]: string | number | boolean; } = {
+        'Access-Control-Allow-Origin': process.env.CORS_ACCESS_CONTROL_ALLOW_ORIGINS,
+        'Content-Type': 'application/json'
+    }
+
     try {
         return {
             statusCode: 200,
             body: JSON.stringify(await generateContentGraph()),
+            headers: headers
         }
     }
     catch (e) {
@@ -13,13 +20,15 @@ export async function getGraph(): Promise<APIGatewayProxyResult> {
             console.error(e.message);
             return {
                 statusCode: 500,
-                body: JSON.stringify(e.message)
+                body: JSON.stringify(e.message),
+                headers: headers
             }
         } else {
             console.error(e);
             return {
                 statusCode: 400,
-                body: 'An unknown error occurred\n' + JSON.stringify(e)
+                body: 'An unknown error occurred\n' + JSON.stringify(e),
+                headers: headers
             }
         }
     }
